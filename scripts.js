93bca8fcf455f27df1d199636c48c9b15b341ac3
Sandra Banaszczyk <hejnowicz.s@gmail.com>
2018-01-03 02:39:32 +0100

    Add scripts

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+alert ('test');
+var a = prompt('Enter the value of the triangle base'),
+    h = prompt('Enter the value of the triangle height'),
+    triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
